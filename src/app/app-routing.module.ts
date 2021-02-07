aedcab7a361158b24089240c63a01e977f548f29
Sebastian Imvinkelried <Sebastian.Imvinkelried@atscom.it>
2021-02-07 12:52:07 -0300

    Comment help admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -68,12 +68,12 @@ const routes: Routes = [
         data: {requiredRoles: [User.RoleEnum.Admin, User.RoleEnum.Consortium]},
         loadChildren: () => import('./pages/results/results.module').then(m => m.ResultsModule)
       },
-      {
-        path: 'help',
-        canActivate: [RoleGuard, AuthGuard],
-        data: {requiredRoles: [User.RoleEnum.Admin]},
-        loadChildren: () => import('./pages/help/help.module').then(m => m.HelpModule)
-      }
+      // {
+      //   path: 'help',
+      //   canActivate: [RoleGuard, AuthGuard],
+      //   data: {requiredRoles: [User.RoleEnum.Admin]},
+      //   loadChildren: () => import('./pages/help/help.module').then(m => m.HelpModule)
+      // }
     ]
   },
   {
