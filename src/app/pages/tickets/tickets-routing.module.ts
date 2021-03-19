01b89eaad79a180e50d87fcc357c672cf1d0b985
Axel <loui.molina1@gmail.com>
2021-03-18 22:02:35 -0300

    Add admin and consortium tickets page separation

--- src/app/pages/tickets/tickets-routing.module.ts
+++ src/app/pages/tickets/tickets-routing.module.ts
@@ -1,10 +1,28 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {ConsotriumTicketsComponent} from './consortium/consotrium-tickets.component';
+import {RoleGuard} from '../../guards/role-guard.service';
+import {AuthGuard} from '../../guards/auth-guard.service';
+import {User} from '../../../../local-packages/banca-api';
 import {TicketsComponent} from './tickets.component';
+import {AdminTicketsComponent} from './admin/admin-tickets.component';
 
 const routes: Routes = [
-  {path: '', component: TicketsComponent},
+  {
+    path: '', component: TicketsComponent
+  },
+  {
+    path: 'admin', component: AdminTicketsComponent,
+    canActivate: [RoleGuard, AuthGuard],
+    data: {requiredRoles: [User.RoleEnum.Admin]},
+  },
+  {
+    path: 'consortium', component: ConsotriumTicketsComponent,
+    canActivate: [RoleGuard, AuthGuard],
+    data: {requiredRoles: [User.RoleEnum.Consortium]},
+  }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
