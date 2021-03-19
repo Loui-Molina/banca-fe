839243ca4c725d3128accc792e03876d468aa3b0
Gonzalo Catrina <gonzalocatrina@gmal.com>
2021-03-19 20:02:44 -0300

    fix url for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   jwtEnabled: true,
-  urlApi: 'http://bsolutions.games/',
-  baseURL: 'http://bsolutions.games/'
+  urlApi: 'https://bsolutions.games',
+  baseURL: 'https://bsolutions.games'
 };
