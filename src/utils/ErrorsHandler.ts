f0bff39e858e2e96fd4bb2456bb5036e1d28b48a
loui <loui.molina1@gmail.com>
2021-04-29 19:13:30 -0400

    Add handling for several equal errors

--- src/utils/ErrorsHandler.ts
+++ src/utils/ErrorsHandler.ts
@@ -23,18 +23,18 @@ export class ErrorsHandler implements ErrorHandler {
     console.log(error);
     if (!navigator.onLine && (error instanceof HttpErrorResponse || ErrorsHandler.chunkFailedMatch.test(error.message))) {
       // NOTE: this way we guess it is an offline
-      this.notifyError(ErrorsHandler.defaultErrorTitle, 'ERROR.ERROR_OFFLINE');
+      this.notifyError(ErrorsHandler.defaultErrorTitle, 'ERROR.ERROR_OFFLINE', 'offline');
       return;
     }
     if (error instanceof HttpErrorResponse) {
       const status = (error as HttpErrorResponse).status;
       switch (status) {
         case 403:
-          this.notifyError(ErrorsHandler.defaultErrorTitle, 'ERROR.NOT_AUTHORIZED');
+          this.notifyError(ErrorsHandler.defaultErrorTitle, 'ERROR.NOT_AUTHORIZED', error.status.toString());
           this.injector.get(Router).navigate(['/'], {replaceUrl: true});
           return;
         case 413:
-          this.notifyError(ErrorsHandler.defaultErrorTitle, 'ERROR.DIMENSION_ERROR');
+          this.notifyError(ErrorsHandler.defaultErrorTitle, 'ERROR.DIMENSION_ERROR', error.status.toString());
           return;
         default:
           break;
@@ -43,23 +43,23 @@ export class ErrorsHandler implements ErrorHandler {
       switch (Math.floor(status / 100)) {
         // 4XX family errors
         case 4:
-          if (error.error && error.error.message){
-           this.notifyError(ErrorsHandler.defaultErrorTitle, error.error.message);
-           return;
+          if (error.error && error.error.message) {
+            this.notifyError(ErrorsHandler.defaultErrorTitle, error.error.message, error.status.toString());
+            return;
           }
-          this.notifyError(ErrorsHandler.defaultErrorTitle, 'ERROR.SYSTEM_ERROR');
+          this.notifyError(ErrorsHandler.defaultErrorTitle, 'ERROR.SYSTEM_ERROR', error.status.toString());
           return;
         // 5XX family errors
         case 5:
-          if (error.error && error.error.message){
-            this.notifyError(ErrorsHandler.defaultErrorTitle, error.error.message);
+          if (error.error && error.error.message) {
+            this.notifyError(ErrorsHandler.defaultErrorTitle, error.error.message, error.status.toString());
             return;
           }
-          this.notifyError(ErrorsHandler.defaultErrorTitle, 'ERROR.SYSTEM_ERROR');
+          this.notifyError(ErrorsHandler.defaultErrorTitle, 'ERROR.SYSTEM_ERROR', error.status.toString());
           return;
         // no errors
         default:
-          this.notifyError(ErrorsHandler.defaultErrorTitle, 'ERROR.ERROR_UNRECOGNIZED');
+          this.notifyError(ErrorsHandler.defaultErrorTitle, 'ERROR.ERROR_UNRECOGNIZED', error.status.toString());
           return;
       }
     } else {
@@ -74,8 +74,8 @@ export class ErrorsHandler implements ErrorHandler {
     this.notifyError(title || ErrorsHandler.defaultErrorTitle, message || ErrorsHandler.genericError, args);
   }
 
-  private notifyError(title: string, message: string, args?: any[]): void {
-    const options: NzNotificationDataOptions<any> = { nzPauseOnHover: true };
+  private notifyError(title: string, message: string, key: string, args?: any[]): void {
+    const options: NzNotificationDataOptions<any> = {nzPauseOnHover: true, nzKey: key };
     this.notification.error(this.ts(title), this.ts(message, args), options);
   }
 
